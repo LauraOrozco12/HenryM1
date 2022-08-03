eb3c3236148f45b3093001618bd8f5cf87296a5f
Laura Orozco <laura.orozco12@hotmail.com>
2022-08-02 20:24:16 -0500

    01-IntroToCS-Homework

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -2,12 +2,23 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  let numArray = Array.from(num);
+  var numArrayReverse = numArray.reverse();
+  let decimal = 0
+  for (let i=0;i<numArrayReverse.length;i++){
+    decimal=decimal+(Math.pow(2,i)*numArrayReverse[i])
+  }
+  return decimal
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  let binario = (num%2).toString()
+  for (;num>1;){
+    num=parseInt(num/2)
+    binario=(num%2)+binario
+  }
+  return binario
 }
 
 
