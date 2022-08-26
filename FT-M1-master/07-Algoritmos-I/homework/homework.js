e37a007a773fd00ff09cd8691e1d3dfc615f90f1
Laura Orozco <laura.orozco12@hotmail.com>
2022-08-26 15:46:17 -0500

    07-Algoritmos-I homework

--- FT-M1-master/07-Algoritmos-I/homework/homework.js
+++ FT-M1-master/07-Algoritmos-I/homework/homework.js
@@ -6,12 +6,23 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-  var primosArr=[2,3,5,7,11,13,11,19,23];
+  // var primosArr=[2,3,5,7,11,13,11,19,23];
+  // var arr=[1];
+  // for(let i=0;i<primosArr.length;i++){
+  //   while(num%primosArr[i]===0){
+  //     arr.push(primosArr[i]);
+  //     num=num/primosArr[i];
+  //   }
+  // }
+  // return arr;
   var arr=[1];
-  for(let i=0;i<primosArr.length;i++){
-    while(num%primosArr[i]===0){
-      arr.push(primosArr[i]);
-      num=num/primosArr[i];
+  var primo=2;
+  while(num>1){
+    if(num%primo===0){
+      arr.push(primo);
+      num=num/primo
+    } else{
+      primo++
     }
   }
   return arr;
@@ -22,25 +33,24 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-  // var arrOrg=[]
-  // while(array.length>0){
-  //   var num = array[0]
-  //   for(let i=0;i<array.length;i++){
-  //     if(num<=array[i]){
-  //       num=array[i];
+  // for(let i =0;i<array.length;i++){
+  //   for(let j=0;j<array.length;j++){
+  //     if(array[j]>array[j+1]){
+  //       var num=array[j];
+  //       array[j]=array[j+1];
+  //       array[j+1]=num;
   //     }
-    
   //   }
-  // arrOrg.unshift(num);
-  // array=array.filter(function(i) { return i !== num })
   // }
-  // return arrOrg;
-  for(let i =0;i<array.length;i++){
+  let swap = true;
+  while(swap){
+    swap=false;
     for(let j=0;j<array.length;j++){
       if(array[j]>array[j+1]){
         var num=array[j];
         array[j]=array[j+1];
         array[j+1]=num;
+        swap=true;
       }
     }
   }
@@ -53,14 +63,21 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-  for(let i=0;i<array.length;i++){
-    for(let j=i;j>0;j--){
-      if(array[j]<array[j-1]){
-        let num=array[j]
-        array.splice(j-1,0,num)
-        array.splice(j+1,1)
-      }
+  for(let i=1;i<array.length;i++){
+    let x=i-1;
+    let temp = array[i];
+    while(x>=0 && temp<array[x]){
+      array[x+1]=array[x];
+      x--;
     }
+    array[x+1]=temp;
+    // for(let j=i;j>0;j--){
+    //   if(array[j]<array[j-1]){
+    //     let num=array[j]
+    //     array.splice(j-1,0,num)
+    //     array.splice(j+1,1)
+    //   }
+    // }
   }
   return array
 }
@@ -85,16 +102,6 @@ function selectionSort(array) {
     array.splice(pos,1);
   }
   return arrOrd;
-    //   var num = array[0]
-    //   for(let i=0;i<array.length;i++){
-    //     if(num<=array[i]){
-    //       num=array[i];
-    //     }
-      
-    //   }
-    // arrOrg.unshift(num);
-    // array=array.filter(function(i) { return i !== num })
-    // }
 }
 
 
