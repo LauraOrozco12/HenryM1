53704ae0c14972bfc8deb8e056675f2001e90eb1
Laura Orozco <laura.orozco12@hotmail.com>
2022-08-30 19:12:19 -0500

    08-Algoritmos-II homework

--- FT-M1-master/08-Algoritmos-II/homework/homework.js
+++ FT-M1-master/08-Algoritmos-II/homework/homework.js
@@ -6,7 +6,25 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  if(array.length<2) return array;
+  if(array.length>1){
+    var pivot = array[Math.floor(array.length/2)]
+    var left=[];
+    var right=[];
+    var arrPivot=[];
+    for(let i=0;i<array.length;i++){
+      if(array[i]<pivot){
+        left.push(array[i])
+      }
+      if(array[i]>pivot){
+        right.push(array[i])
+      }
+      if(array[i]===pivot){
+        arrPivot.push(array[i])
+      }
+    }
+  }
+  return quickSort(left).concat(arrPivot,quickSort(right))
 }
 
 function mergeSort(array) {
@@ -14,9 +32,28 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  if(array.length<2) return array;
+  if(array.length>1){
+    var pivot = Math.floor(array.length/2)
+    var left = array.slice(0, pivot);
+    var right = array.slice(pivot);
+  }
+  return merge(mergeSort(left), mergeSort(right));
 }
 
+function merge(left, right){
+  var arrOrd=[]
+  while (left.length>0 && right.length>0){
+    if(left[0]<right[0]){
+      arrOrd.push(left.shift());
+    } else {
+      arrOrd.push(right.shift());
+    }
+  }
+  return arrOrd.concat(left,right);
+}
+
+mergeSort([4,9,1,3,2])
 // No modificar nada debajo de esta línea
 // --------------------------------
 
